f246c7b663436be876cf372520a684f55291a258
Yoel Monzon <yoelfme@hotmail.com>
2016-02-26 23:56:50 -0600

    [add] class for test dependencias and coupled code

---
+++ src/Authenticator.php
@@ -0,0 +1,28 @@
+<?php
+namespace Styde;
+
+class Authenticator
+{
+
+    protected static $user;
+
+    public static function check()
+    {
+        return static::user() != null;
+    }
+
+    public static function user()
+    {
+        if (static::$user != null) {
+            return static::$user;
+        }
+
+        $data = Session::get('user_data');
+
+        if (! is_null($data)) {
+            return static::$user = new User($data);
+        }
+
+        return null;
+    }
+}
