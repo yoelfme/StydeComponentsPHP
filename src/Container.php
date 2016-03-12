4045db7fe5dc0b5d2913efb2d795f29a8166ae9e
Yoel Monzon <yoelfme@hotmail.com>
2016-03-12 00:19:28 -0600

    [add] function to implement reflection class for dependency injection

--- src/Container.php
+++ src/Container.php
@@ -2,6 +2,7 @@
 namespace Styde;
 
 use Closure;
+use ReflectionClass;
 
 class Container
 {
@@ -32,9 +33,38 @@ class Container
         if ($resolver instanceof Closure) {
             $object = $resolver($this);
         } else {
-            $object = new $resolver;
+            $object = $this->build($resolver);
         }
 
         return $object;
+    }
+
+    public function build($name)
+    {
+        $reflection = new ReflectionClass($name);
+
+        if (!$reflection->isInstantiable()) {
+            throw new InvalidArgumentException($name.' is not instantiable');
+        }
+
+        $constructor = $reflection->getConstructor(); // ReflectionMethod
+
+        if (is_null($constructor)) {
+            return new $name;
+        }
+
+        $constructorParameters = $constructor->getParameters(); // ReflectionParameter
+
+        $arguments = [];
+
+        foreach ($constructorParameters as $constructorParameter) {
+            $parameterClassName = $constructorParameter->getClass()->getName();
+
+            $arguments[] = $this->build($parameterClassName);
+        }
+
+        // new Foo($bar)
+        return $reflection->newInstanceArgs($arguments);
+
     }
 }
